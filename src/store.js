99909e61fb62a71cd8906f24542b0b3affc288f6
yanxi1231 <na.ya@northeastern.edu>
2025-08-08 23:57:15 -0400

    final

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
 import avReducer from './avSlice';
+import mealReducer from './mealsSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
     av: avReducer,
+    meals: mealReducer,
   },
 });
